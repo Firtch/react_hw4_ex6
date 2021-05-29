afd1f38f5c5f1e69df932abe366c13012ea4ca64
Firtch <firtch@gmail.com>
2021-05-29 03:24:21 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // 0 = 0
+  // 1 = x
+  // 2 = пусто
+  field = [
+    [2, 2, 2],
+    [2, 2, 2],
+    [2, 2, 2],
+  ];
+
+  state = {
+    xTurn: true,
+  };
+
+  onClickButton = (e) => {    
+    const pos = e.target.id.split(",")
+    const posX = pos[0];
+    const posY = pos[1];
+    
+    if(this.field[parseInt(pos[0])][parseInt(pos[1])] >= 2) {
+      let xTurn = this.state.xTurn;
+      if(this.state.xTurn) {
+        this.field[posX][posY] = 1;
+        e.target.innerText = "X";
+        xTurn = !xTurn;        
+      } else {
+        this.field[posX][posY] = 0;
+        e.target.innerText = "0";
+        xTurn = !xTurn;
+      }
+
+      console.log(this.field);
+
+      if(this.field[0][0] === this.field[0][1] && this.field[0][0] === this.field[0][2] && this.field[0][0] < 2 ||
+        this.field[1][0] === this.field[1][1] && this.field[1][0] === this.field[1][2] && this.field[1][0] < 2 ||
+        this.field[2][0] === this.field[2][1] && this.field[2][0] === this.field[2][2] && this.field[2][0] < 2 ||
+        this.field[0][0] === this.field[1][0] && this.field[0][0] === this.field[2][0] && this.field[0][0] < 2 ||
+        this.field[0][1] === this.field[1][1] && this.field[0][1] === this.field[2][1] && this.field[0][1] < 2 ||
+        this.field[0][0] === this.field[1][1] && this.field[0][0] === this.field[2][2] && this.field[0][0] < 2 || 
+        this.field[2][0] === this.field[1][1] && this.field[2][0] === this.field[2][0] && this.field[2][0] < 2) {
+          alert(this.state.xTurn ? "X winner" : "0 winner");
+          window.location.reload(false);
+        }
+      
+      this.setState({xTurn});
+      
+      
+    }
+  
+  
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <table>
+          <tr>
+            <td>
+              <button id="0,0" onClick={this.onClickButton}></button>
+            </td>
+            <td>
+              <button id="0,1" onClick={this.onClickButton} />
+            </td>
+            <td>
+              <button id="0,2" onClick={this.onClickButton} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button id="1,0"  onClick={this.onClickButton} />
+            </td>
+            <td>
+              <button id="1,1"  onClick={this.onClickButton} />
+            </td>
+            <td>
+              <button id="1,2"  onClick={this.onClickButton} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button id="2,0"  onClick={this.onClickButton} />
+            </td>
+            <td>
+              <button id="2,1"  onClick={this.onClickButton} />
+            </td>
+            <td>
+              <button id="2,2"  onClick={this.onClickButton} />
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
